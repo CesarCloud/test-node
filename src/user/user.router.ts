8e01e8bf18c6b16dee2d1c8fc35f8099974d5d4c
CesarCloud <77102585@qq.com>
2021-08-09 22:34:01 +0800

    用户验证

--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 import * as userController from './user.controller';
+import { validateUserData } from './user.middleware';
 
 const router =express.Router();
 
+/**
+ * 创建用户
+ */
+router.post('/users',validateUserData,userController.store);
 /**
  * 导出路由
  */
