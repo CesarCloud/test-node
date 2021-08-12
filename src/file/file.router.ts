4d68c702b4a427f32f6cb305b263675c019b673c
CesarCloud <77102585@qq.com>
2021-08-12 23:46:39 +0800

    设置文件接口

--- src/file/file.router.ts
+++ src/file/file.router.ts
@@ -1,8 +1,18 @@
 import express from 'express';
+import { authGuard } from '../auth/auth.middleware';
 import * as fileController from './file.controller';
+import { fileInterceptor } from './file.middleware';
 
 const router =express.Router();
 
+/**
+ * 上传文件
+ */
+router.post('/files',authGuard,fileInterceptor,fileController.store);
+/**
+ * 文件服务
+ */
+router.get('/files/:fileId/serve', fileController.serve);
 /**
  * 导出路由
  */
