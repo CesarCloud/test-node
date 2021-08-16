343ae4180da8e42409850782323ada2c0b1ab72c
CesarCloud <77102585@qq.com>
2021-08-16 23:56:40 +0800

    定义点赞和取消点赞的接口

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -39,11 +39,13 @@ export const getPosts =async (options:GetPostsOptions)=>{
         ${sqlFragment.user},
         ${sqlFragment.totalComments},
         ${sqlFragment.file},
-        ${sqlFragment.tags}
+        ${sqlFragment.tags},
+        ${sqlFragment.totalLikes}
     FROM post
         ${sqlFragment.leftJoinUser}
         ${sqlFragment.leftJionOneFile}
         ${sqlFragment.leftJoinTag}
+        ${filter.name='userLiked'? sqlFragment.innerJoinUserLikePost:''}
     WHERE ${filter.sql}
     GROUP BY post.id
     ORDER BY ${sort}
@@ -169,6 +171,7 @@ export const postHasTag =async(
         ${sqlFragment.leftJoinUser}
         ${sqlFragment.leftJionOneFile}
         ${sqlFragment.leftJoinTag}
+        ${filter.name='userLiked'? sqlFragment.innerJoinUserLikePost:''}
         WHERE ${filter.sql}
     `;
     //执行查询
